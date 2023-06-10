b266ef71b25c078bd1d87278fbbabcdde2a2a271
Vasant Saladi <60614334+vasantsaladi@users.noreply.github.com>
2023-06-10 16:29:57 -0400

    Update fire.js

--- client/src/fire.js
+++ client/src/fire.js
@@ -5,12 +5,12 @@ import "firebase/storage";
 import "firebase/firestore";
 
 const firebaseConfig = {
-	apiKey: "AIzaSyCEfp-raM8Qqc_Al8xFRNgwqVyu2suT8HY",
-	authDomain: "tech-incubator-new.firebaseapp.com",
-	projectId: "tech-incubator-new",
-	storageBucket: "tech-incubator-new.appspot.com",
-	messagingSenderId: "822233257343",
-	appId: "1:822233257343:web:03394d13533340f1ecb64a",
+    apiKey: "AIzaSyCEfp-raM8Qqc_Al8xFRNgwqVyu2suT8HY",
+    authDomain: "tech-incubator-new.firebaseapp.com",
+    projectId: "tech-incubator-new",
+    storageBucket: "tech-incubator-new.appspot.com",
+    messagingSenderId: "822233257343",
+    appId: "1:822233257343:web:03394d13533340f1ecb64a",
 };
 
 const fire = firebase.initializeApp(firebaseConfig);
