121979043ccff7f9c29b7951b12777b913d8270a
iyasin <iayasin771@gmail.com>
2023-06-10 21:21:43 -0400

    Added buttons to posts

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -1,9 +1,11 @@
 import "./Post.css";
+import { useState, useEffect } from "react";
 
 import React from "react";
 
 const Post = ({ task }) => {
 	// console.log(task);
+	const [state, setState] = useState(1);
 
 	if (!task.hasOwnProperty("Creator")) {
 		return;
@@ -14,6 +16,21 @@ const Post = ({ task }) => {
 			<div className="square">
 				<h1>{task.Creator}</h1>
 				<p>{task.Description}</p>
+				{state === 1 && (
+					<button className="accept" onClick={() => setState(2)}>
+						<p>Accept</p>
+					</button>
+				)}
+				{state === 2 && (
+					<button className="upload" onClick={() => setState(3)}>
+						Upload
+					</button>
+				)}
+				{state === 3 && (
+					<button className="submit" onClick={() => setState(1)}>
+						Submit
+					</button>
+				)}
 			</div>
 		</>
 	);
