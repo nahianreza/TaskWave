bb4efceb98385b3d8890138c0dc7c5317424e0f9
Vasant Saladi <60614334+vasantsaladi@users.noreply.github.com>
2023-06-12 20:23:44 -0400

    updated post
    made a submitting link funtion and estimated time function

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -6,10 +6,32 @@ import React from "react";
 const Post = ({ task }) => {
 	// console.log(task);
 	const [state, setState] = useState(1);
+	const [url, setUrl] = useState("");
+	const [estimatedTime, setEstimatedTime] = useState("");
 
 	if (!task.hasOwnProperty("Creator")) {
 		return;
 	}
+
+	const handleUrlChange = (event) => {
+		setUrl(event.target.value);
+	  };
+	
+	  const handleUrlSubmit = () => {
+		// Process the submitted URL here
+		console.log(url);
+		setState(1);
+	  };
+	
+	const handleEstimatedTimeChange = (event) => {
+		setEstimatedTime(event.target.value);
+	  };
+	
+	const handleEstimatedTimeSubmit = () => {
+		// Process the submitted estimated time here
+		console.log(estimatedTime);
+		setState(1);
+	  };
 
 	return (
 		<>
@@ -17,19 +39,43 @@ const Post = ({ task }) => {
 				<h1>{task.Creator}</h1>
 				<p>{task.Description}</p>
 				{state === 1 && (
-					<button className="accept" onClick={() => setState(2)}>
-						<p>Accept</p>
-					</button>
+					<>
+						<input
+							type="text"
+							placeholder="Estimated Time"
+							value={estimatedTime}
+						o	nChange={handleEstimatedTimeChange}
+						/>	
+						<button className="accept" onClick={() => setState(2)}>
+							<p>Accept</p>
+						</button>
+					</>
 				)}
 				{state === 2 && (
-					<button className="upload" onClick={() => setState(3)}>
-						Upload
-					</button>
+					<>
+						<input
+							type="text"
+							placeholder="Enter Git Hub URL"
+							value={url}
+							onChange={handleUrlChange}
+						/>	
+						<button className="upload" onClick={() => setState(3)}>
+							Upload
+						</button>
+					</>
 				)}
 				{state === 3 && (
-					<button className="submit" onClick={() => setState(1)}>
-						Submit
-					</button>
+					<>
+						<input
+							type="text"
+							placeholder="Enter Git Hub URL"
+							value={url}
+							onChange={handleUrlChange}
+						/>	
+						<button className="submit" onClick={() => setState(1)}>
+							Submit
+						</button>
+					</>
 				)}
 			</div>
 		</>
